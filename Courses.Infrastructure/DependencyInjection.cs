136d88087e610a2fc4d8fc6ed25ac5d744170293
Mostafa Mahmoud <mostafapro87@gmail.com>
2026-05-04 23:10:32 +0300

    Add Courses.Infrastructure project with DI setup.

---
+++ Courses.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Courses.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCoursesInfrastructure(this IServiceCollection services)
+    {
+        return services;
+    }
+}
