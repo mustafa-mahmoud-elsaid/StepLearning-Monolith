585cce32d25ea3d3593c32c5a68a7a63eadb1a6a
Mostafa Mahmoud <mostafapro87@gmail.com>
2026-05-05 19:26:01 +0300

    Add entites configs & register db in DI container.

--- Courses.Infrastructure/DependencyInjection.cs
+++ Courses.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Courses.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Courses.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddCoursesInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddCoursesInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<CoursesDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("CoursesDbConnection"), sql =>
+            {
+                sql.EnableRetryOnFailure(2);
+            });
+        });
+
         return services;
     }
 }
