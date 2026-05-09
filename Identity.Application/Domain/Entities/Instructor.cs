fb62565da8a11f2990f94bf9efb1137ad6cebcad
Mostafa Mahmoud <mostafapro87@gmail.com>
2026-05-09 23:04:35 +0300

    Add missing property.

--- Identity.Application/Domain/Entities/Instructor.cs
+++ Identity.Application/Domain/Entities/Instructor.cs
@@ -7,8 +7,9 @@ public class Instructor
     public Guid UserId { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
+    public string? ProfilePictureUrl { get; set; }
 
-    public string FullName => string.Concat(FirstName, " ", LastName);
+    public string FullName => $"{FirstName} {LastName}";
     public string? Bio { get; set; }
 
 }
