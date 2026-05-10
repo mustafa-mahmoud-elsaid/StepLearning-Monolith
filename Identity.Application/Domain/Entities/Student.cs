a7abeef5ac535a16c48477072cc388ab556848dc
Mostafa Mahmoud <mostafapro87@gmail.com>
2026-05-10 05:44:21 +0300

    Add Create Entity & Make the domain properties private set.

--- Identity.Application/Domain/Entities/Student.cs
+++ Identity.Application/Domain/Entities/Student.cs
@@ -2,10 +2,28 @@
 
 public class Student
 {
-    public Guid Id { get; set; }
-    public string FullName { get; set; } = string.Empty;
-    public Guid UserId { get; set; }
-    public DateOnly? DateOfBirth { get; set; }
-    public string? ProfilePictureUrl { get; set; }
-    public DateTime CreatedAt { get; set; }
+    public Guid Id { get; private set; }
+    public string FullName { get; private set; } = string.Empty;
+    public Guid UserId { get; private set; }
+    public DateOnly? DateOfBirth { get; private set; }
+    public string? ProfilePictureUrl { get; private set; }
+
+    public static Student Create(string fullName,  Guid userId, DateOnly? dateOfBirth, string? profilePictureUrl)
+    {
+        if (string.IsNullOrWhiteSpace(fullName))
+        {
+            throw new InvalidOperationException("fullName must have actual value");
+        }
+        if (userId == Guid.Empty)
+        {
+            throw new InvalidOperationException("User id must not be empty");
+        }
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            UserId = userId,
+            DateOfBirth = dateOfBirth,
+            ProfilePictureUrl = profilePictureUrl
+        };
+    }
 }
