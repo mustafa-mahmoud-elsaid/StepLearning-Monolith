1c54a612cc647dc767d4fdfcc2dd2ced82559e08
Mostafa Mahmoud <mostafapro87@gmail.com>
2026-05-12 00:50:49 +0300

    Upgrade swagger package.

--- Host.Api/Program.cs
+++ Host.Api/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    options.SwaggerDoc("v1", new()
     {
         Title = "StepLearning API",
         Version = "v1",
