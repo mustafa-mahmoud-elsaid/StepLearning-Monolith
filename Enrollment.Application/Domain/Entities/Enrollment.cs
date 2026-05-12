713284a7a3e6eed619e1d226a8f547ad7d534796
Mostafa Mahmoud <mostafapro87@gmail.com>
2026-05-12 11:23:32 +0300

    Add Domain Entites & Install required packages

---
+++ Enrollment.Application/Domain/Entities/Enrollment.cs
@@ -0,0 +1,30 @@
+﻿using Enrollment.Application.Domain.Enums;
+
+namespace Enrollment.Application.Domain.Entities;
+
+public class Enrollment
+{
+    public Guid Id { get; private set; }
+    public Guid StudentId { get; private set; }
+    public Guid CourseId { get; private set; }
+    public DateTime EnrolledAt { get; private set; }
+    public EnrollmentStatus Status { get; private set; }
+
+    public static Enrollment Create(Guid studentId, Guid courseId,  EnrollmentStatus status)
+    {
+        if (studentId == Guid.Empty)
+            throw new InvalidOperationException("Student id can not be empty");
+
+        if (courseId == Guid.Empty)
+            throw new InvalidOperationException("Course id can not be empty");
+
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            StudentId = studentId,
+            CourseId = courseId,
+            EnrolledAt = DateTime.UtcNow,
+            Status = status
+        };
+    }
+}
---
+++ Enrollment.Application/Domain/Enums/EnrollmentStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Enrollment.Application.Domain.Enums;
+
+public enum EnrollmentStatus
+{
+    Active,
+    Completed,
+    Cancelled,
+    Suspended,
+    Expired
+}
